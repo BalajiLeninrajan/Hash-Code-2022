1c9fcb62412e88164c66add51481fda14ceac50a
Balaji The Human <bleninrajan@gmail.com>
2022-02-25 01:18:20 +0700

    add boilerplate

---
+++ projects.py
@@ -0,0 +1,4 @@
+class Projects:
+    def __init__(self, name, skills):
+        self.name = name
+        self.skills = skills
---
+++ skills.py
@@ -0,0 +1,7 @@
+from logging import _Level
+
+
+class skills:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
